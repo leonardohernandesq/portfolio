9ff3c19c1fd9a45617af75b6cf977c8568720848
Leonardo Hernandes <62108109+leonardohernandesq@users.noreply.github.com>
2024-12-20 10:52:23 +0000

    fix: refactor SEO code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import * as S from '../../styles/pages/home'
-
 import Link from 'next/link';
 
 import { Footer } from "../components/Footer";
@@ -11,10 +10,18 @@ import { FaLinkedinIn, FaGithub, FaWhatsapp } from 'react-icons/fa';
 import { Experience } from '../components/Experience';
 import { Projects } from '../components/Projects';
 import { Contact } from '../components/Contact';
+import Head from 'next/head';
 
 export default function Home(){
   return(
     <>
+      <Head>
+        <title>Leonardo Hernandes - Desenvolvedor Frontend</title>
+        <meta name="description" content="Desenvolvedor Frontend especializado em criar interfaces web dinâmicas, 
+          responsivas e de alto desempenho. Com experiência em HTML, CSS, JavaScript, React e outras tecnologias modernas, 
+          foco na entrega de experiências de usuário excepcionais."
+        />
+      </Head>
       <Header />
         <S.Container> 
           <S.Banner>
@@ -26,15 +33,15 @@ export default function Home(){
               </h1>
               <h2>Frontend Developer</h2>
               <S.SocialMedia>
-                <Link href='https://www.linkedin.com/in/leonardo-hernandes/' target='blank'>
+                <Link aria-label='Conheça o meu Linkedin' href='https://www.linkedin.com/in/leonardo-hernandes/' target='blank'>
                   <div className='border'></div>
                   <FaLinkedinIn size={30}/>
                 </Link>
-                <Link href='https://github.com/leonardohernandesq' target='blank'>
+                <Link aria-label='Conheça o meu GitHub' href='https://github.com/leonardohernandesq' target='blank'>
                   <div className='border'></div>
                   <FaGithub size={30}/>
                 </Link>
-                <Link href='https://api.whatsapp.com/send/?phone=351922269284&type=phone_number&app_absent=0' target='blank'>
+                <Link aria-label='Fale comigo no Whatsapp' href='https://api.whatsapp.com/send/?phone=351922269284&type=phone_number&app_absent=0' target='blank'>
                   <div className='border'></div>
                   <FaWhatsapp size={30}/>
                 </Link>
